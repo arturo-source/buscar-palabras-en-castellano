db94a9505ba2993c97aff461dad92983a036d5c2
ArturoSource <arturojuan9898@gmail.com>
2022-02-27 11:49:02 +0100

    Add first and last buttons

--- pagination.js
+++ pagination.js
@@ -5,6 +5,8 @@ class Paginator {
     rowsPerPage = 4,
     prevBtn,
     nextBtn,
+    firstBtn,
+    lastBtn,
   }) {
     this.elements = elements;
 
@@ -15,35 +17,59 @@ class Paginator {
 
     this.prevBtn = prevBtn;
     this.nextBtn = nextBtn;
+    this.firstBtn = firstBtn;
+    this.lastBtn = lastBtn;
 
-    this.prevBtn.onclick = () => {
-      if (this.currentPage > 0) {
-        this.currentPage--;
-        this.disable();
-      }
-    };
-    this.nextBtn.onclick = () => {
-      if (this.currentPage < this.lastPage) {
-        this.currentPage++;
-        this.disable();
-      }
-    };
+    if (this.prevBtn != undefined) this.prevBtn.onclick = this.goToPrevPage;
+    if (this.nextBtn != undefined) this.nextBtn.onclick = this.goToNextPage;
+    if (this.firstBtn != undefined) this.firstBtn.onclick = this.goToFirstPage;
+    if (this.lastBtn != undefined) this.lastBtn.onclick = this.goToLastPage;
   }
 
   disable() {
-    if (this.currentPage == 0) this.prevBtn.setAttribute("disabled", "");
-    else this.prevBtn.removeAttribute("disabled", "");
+    if (this.currentPage == 0) {
+      this.prevBtn?.setAttribute("disabled", "");
+      this.firstBtn?.setAttribute("disabled", "");
+    } else {
+      this.prevBtn?.removeAttribute("disabled", "");
+      this.firstBtn?.removeAttribute("disabled", "");
+    }
 
-    if (this.currentPage == this.lastPage || this.lastPage == -1)
-      this.nextBtn.setAttribute("disabled", "");
-    else this.nextBtn.removeAttribute("disabled", "");
+    if (this.currentPage == this.lastPage || this.lastPage == -1) {
+      this.nextBtn?.setAttribute("disabled", "");
+      this.lastBtn?.setAttribute("disabled", "");
+    } else {
+      this.nextBtn?.removeAttribute("disabled", "");
+      this.lastBtn?.removeAttribute("disabled", "");
+    }
+  }
+
+  goToPrevPage() {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.disable();
+    }
+  }
+  goToNextPage() {
+    if (this.currentPage < this.lastPage) {
+      this.currentPage++;
+      this.disable();
+    }
+  }
+  goToFirstPage() {
+    this.currentPage = 0;
+    this.disable();
+  }
+  goToLastPage() {
+    this.currentPage = this.lastPage;
+    this.disable();
   }
 
   get cellsPerPage() {
     return this.rowsPerPage * this.cellsPerRow;
   }
   get lastPage() {
-    return Math.ceil(this.elements.length / cellsPerPage) - 1;
+    return Math.ceil(this.elements.length / this.cellsPerPage) - 1;
   }
   get currentElements() {
     let next_pos = this.currentPage * this.cellsPerPage;
@@ -69,6 +95,8 @@ class Paginator {
 window.onload = () => {
   prevBtn = document.getElementById("prev"); // Introduce btn ID
   nextBtn = document.getElementById("next"); // Introduce btn ID
+  firstBtn = document.getElementById("first"); // Introduce btn ID
+  lastBtn = document.getElementById("last"); // Introduce btn ID
 
   var paginator = new Paginator({
     elements: [2, 3, 65, "sd", "dfo", "95j", 3],
